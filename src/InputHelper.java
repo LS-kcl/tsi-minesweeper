0f8ed44a648439be8fe4270c6a0e9e53662c37ea
Luke <lbmoogs@gmail.com>
2024-04-03 15:02:42 +0100

    add: Add method content to InputHelper

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 public class InputHelper {
-  public static String stringInput() {
-    // TO IMPLEMENT
-    return "Hello World";
+  private static Scanner input = new Scanner(System.in);
+  public static String getStringInput(){
+     return input.nextLine();
+  }
+
+  public static int getIntInput(){
+    // Loop until valid number to return
+    while (true) {
+            String str = input.nextLine();
+            int res = 0;
+            try {
+                res = Integer.parseInt(str);
+                return res;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number, please try again:");
+            }
+        }
   }
 }
