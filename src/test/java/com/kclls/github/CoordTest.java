9f86f76602af41658720cbd4bb1550b3c7355c93
Luke <lbmoogs@gmail.com>
2024-04-05 11:18:54 +0100

    add: Add test for Coord class

--- src/test/java/com/kclls/github/CoordTest.java
+++ src/test/java/com/kclls/github/CoordTest.java
@@ -2,12 +2,37 @@ package com.kclls.github;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CoordTest {
+    Coord coord;
+    @BeforeAll
+    public void setUp (){
+      coord = new Coord(1, 2);
+    }
 
     @Test
     public void canAccessCoordColDirectly(){
-        Coord c = new Coord(1,1);
-        Assertions.assertEquals(1, c.col);
+        int expected = 2;
+        Assertions.assertEquals(expected, coord.col, "Must be able to access Coord col");
     }
+
+    @Test
+    public void canAccessCoordRowDirectly(){
+        int expected = 1;
+        Assertions.assertEquals(expected, coord.row, "Must be able to access Coord row");
+    }
+
+    @Test
+    public void stringOutputsCorrectly() {
+        String expected = "(1, 2)";
+        Assertions.assertEquals(expected.equals(coord.getStrVersion()), "Coord is not returning the correct string");
+    }
+
+    /*
+     * coord.row and coord.col are immutable, and throw compile errors
+     * if we attempt to set a new value
+     */
 }
